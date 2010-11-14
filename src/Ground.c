63165259388ddd3435642a484c30b11b5d1aa26a
Amirouche Halfaoui <amihalfa@users.noreply.github.com>
2010-11-14 12:03:34 +0000

    Modifications pour les textures

--- src/Ground.c
+++ src/Ground.c
@@ -1,6 +1,6 @@
-#include "includes/Ground.h"
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include "includes/Ground.h"
 
 /**
  *	Dessin du terrain en OpenGL
@@ -10,33 +10,57 @@ void ground_draw( Ground* ground ){
 	
 	int i , j;
 	float length_mi , width_mi;
+	float text_i=0, text_j=0;
 	
 	/* Pour centrer le terrain, on divise les tailles par 2 */
 	length_mi = ground->length / 2;
 	width_mi = ground->width / 2;
 	
+	glEnable (GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, ground->texture);
+	
 	/* Début de dessin */
 	glBegin( GL_QUADS );
 	
 	/* Couleur pour le sol */
-	glColor3ub( 210 , 200 , 200 );
+	glColor3ub( 255 , 255 , 255 );
 	
 	glNormal3f( 0.0f , 0.0f , 1.0f );
 	
-	/* Dessin du sol */
+	glTexCoord2i(0,1);
+	glVertex3d( -width_mi , -length_mi , 0 );
+	
+	glTexCoord2i(1,1);
+	glVertex3d( width_mi , -length_mi, 0 );
+	
+	glTexCoord2i(1,0);
+	glVertex3d( width_mi , length_mi , 0 );
+	
+	glTexCoord2i(0,0);
+	glVertex3d( -width_mi , length_mi , 0 );
+	
+	
+	/* Dessin du sol 
 	for(i = -width_mi; i < width_mi; i+=1){
 		for(j = -length_mi; j < length_mi; j+=1){
+			glTexCoord2i(i,j+1);
 			glVertex3d( i , j , 0 );
+			
+			glTexCoord2i(i+1,j+1);
 			glVertex3d( i+1 , j, 0 );
+			
+			glTexCoord2i(i+1,j);
 			glVertex3d( i+1 , j+1, 0 );
+			
+			glTexCoord2i(i,j);
 			glVertex3d( i , j+1 , 0 );
 		}
 	}
 	
 	/* Couleur des faces */
-	glColor3ub( 150 , 100 , 100 );
+	glColor3ub( 155 , 155 , 155 );
 	
-	/* Dessin de la face arriere */
+	/* Dessin de la face arriere 
 	glNormal3f( 0.0f , -1.0f , 0.0f );
 	for(i = -width_mi; i < width_mi; i++){
 		glVertex3d( i+1 , length_mi , 0 );
@@ -45,7 +69,7 @@ void ground_draw( Ground* ground ){
 		glVertex3d( i , length_mi , 0 );
 	}
 		
-	/* Dessin de la face avant */
+	/* Dessin de la face avant 
 	glNormal3f( 0.0f , 1.0f , 0.0f );
 	for(i = -width_mi; i < width_mi; i++){
 		glVertex3d( i+1 , -length_mi , 0 );
@@ -71,5 +95,5 @@ void ground_draw( Ground* ground ){
 		glVertex3d( width_mi , i , 0 );
 	}
 	glEnd();
-	
+	glDisable (GL_TEXTURE_2D);
 }

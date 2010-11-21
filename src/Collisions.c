e531470b82f7d95d0028d9c32da1ac896bd00555
amiroucheh <amiroucheh@c265c30d-8ff1-4933-a734-8ff9be2d7a7d>
2010-11-21 13:22:26 +0000

    Ajout de la Doc

--- src/Collisions.c
+++ src/Collisions.c
@@ -12,7 +12,7 @@ int collision_state_game(State_Game_Env* env, Uint32 e_time){
 	if(collision_ball_racket(&env->ball, &env->racket_bottom) == 2){
 		ball_move(&env->ball, e_time);
 	}
-	if(collision_ball_racket(&env->ball, &env->racket_top)){
+	if(collision_ball_racket(&env->ball, &env->racket_top) == 2){
 		ball_move(&env->ball, e_time);
 	}
 	collision_ball_ground(&env->ball, &env->ground);
@@ -37,24 +37,33 @@ int collision_racket_ground( Racket* racket, Ground* ground, int position ){
 
 }
 
-void collision_ball_ground( Ball* ball, Ground* ground ){
+int collision_ball_ground( Ball* ball, Ground* ground ){
 
+	/* Collision a gauche du terrain */
 	if( ball->x - ball->radius < -ground->width / 2 ){
-		
 		ball->speed_x *= -1;
 		ball->x = ball->radius -ground->width / 2;
+		
+		return 1;
+	} 
 	
-	} else if( ball->x + ball->radius > ground->width / 2 ){
-	
+	/* Collision a droite du terrain */
+	else if( ball->x + ball->radius > ground->width / 2 ){
 		ball->speed_x *= -1;
 		ball->x = ground->width / 2 - ball->radius;
 		
-	} else if( ball->y + ball->radius > ground->length / 2 ){
+		return 1;
+	} 
+	
+	/* Collision par le haut ... C'est temporaire ... */
+	else if( ball->y + ball->radius > ground->length / 2 ){
 		
 		ball->speed_y *= -1;
 		ball->y = ground->length / 2 - ball->radius;
 		
+		return 2;
 	}
+	return 0;
 	
 }
 
@@ -111,6 +120,7 @@ int collision_ball_racket( Ball* ball, Racket* racket ){
 				ball->speed_y += 2 * d_o * u_y;
 				return 2;
 			}
+			
 			/* La raquette a ete cognee par la droite */
 			else if(dist_right <= ball->radius + racket->radius){
 				
@@ -134,4 +144,54 @@ int collision_ball_racket( Ball* ball, Racket* racket ){
 	}
 	return 0;
 	
+}
+
+int collision_ball_ball( Ball* ball_a, Ball* ball_b ){
+
+	float dist_x = ball_a->x - ball_b->x;
+	float dist_y = ball_a->y - ball_b->y;
+	
+	/* Distance entre les deux balles */
+	float dist_bb = sqrt( dist_x*dist_x + dist_y*dist_y );
+	
+	/* Cas ou il y a collision */
+	if(dist_bb <= ball_a->radius + ball_b->radius ){
+	
+		/* On devie d'abord la balle A de sa trajectoire */
+		float u_x = dist_x / dist_bb;
+		float u_y = dist_y / dist_bb;
+		
+		float d_x = ball_a->speed_x;
+		float d_y = ball_a->speed_y;
+		
+		float o_x = u_x * (u_x*d_x + u_y*d_y) / (u_x*u_x + u_y*u_y);
+		float o_y = u_y * (u_x*d_x + u_y*d_y) / (u_x*u_x + u_y*u_y);
+		
+		float d_o = sqrt(o_x*o_x + o_y*o_y);
+		
+		ball_a->speed_x += 2 * d_o * u_x;
+		ball_a->speed_y += 2 * d_o * u_y;
+		
+		/* On devie la balle B de sa trajectoire */
+		u_x = -u_x;
+		u_y = -u_y;
+		
+		d_x = ball_b->speed_x;
+		d_y = ball_b->speed_y;
+		
+		o_x = u_x * (u_x*d_x + u_y*d_y) / (u_x*u_x + u_y*u_y);
+		o_y = u_y * (u_x*d_x + u_y*d_y) / (u_x*u_x + u_y*u_y);
+		
+		d_o = sqrt(o_x*o_x + o_y*o_y);
+		
+		ball_b->speed_x += 2 * d_o * u_x;
+		ball_b->speed_y += 2 * d_o * u_y;
+		
+		return 1;
+	}
+	
+	/* Pas de collision entre les balles */
+	else {
+		return 0;
+	}
 }

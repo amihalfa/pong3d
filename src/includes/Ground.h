d680cb394ca174b8390ed043a7901bbcfab01ac9
amiroucheh <amiroucheh@c265c30d-8ff1-4933-a734-8ff9be2d7a7d>
2010-12-21 14:10:47 +0000

    Particules et leur gestion !

--- src/includes/Ground.h
+++ src/includes/Ground.h
@@ -9,38 +9,38 @@
 #ifndef GROUND_H
 #define GROUND_H
 
-	/**
-	*	Structure permettant de gerer le terrain
-	*/
-	typedef struct Ground{
-		
-		/** Hauteur du terrain */
-		float height;
-		
-		/** Longueur du terrain */
-		float length;
-		
-		/** Largeur du terrain */
-		float width;
-		
-		/** Identifiants du buffer */
-		GLuint buffer[2];
-		
-	} Ground;
-	
-	/**
-	 * Initialisation du dessin du terrain en OpenGL
-	 * On prépare les donnees a envoyer a la carte graphique
-	 * @param	ground		Pointeur vers la structure a utiliser designant le terrain
-	 */		
-	void ground_init_draw( Ground* ground);
-	
-	/**
-	 *	Dessin du terrain en OpenGL
-	 *	@param	ground		Pointeur vers la structure a utiliser pour le dessin
-	 */
-	void ground_draw( Ground* ground );
-	
+/**
+*	Structure permettant de gerer le terrain
+*/
+typedef struct Ground {
+
+    /** Hauteur du terrain */
+    float height;
+
+    /** Longueur du terrain */
+    float length;
+
+    /** Largeur du terrain */
+    float width;
+
+    /** Identifiants du buffer */
+    GLuint buffer[2];
+
+} Ground;
+
+/**
+ * Initialisation du dessin du terrain en OpenGL
+ * On prépare les donnees a envoyer a la carte graphique
+ * @param	ground		Pointeur vers la structure a utiliser designant le terrain
+ */
+void ground_init_draw( Ground* ground );
+
+/**
+ *	Dessin du terrain en OpenGL
+ *	@param	ground		Pointeur vers la structure a utiliser pour le dessin
+ */
+void ground_draw( Ground* ground );
+
 
 #endif
 

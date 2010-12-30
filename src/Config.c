0281171c541c43afd0f7b3c6f3df6c1cd86c8c5e
Amirouche Halfaoui <amihalfa@users.noreply.github.com>
2010-12-30 15:35:05 +0000

    dklfjskl

--- src/Config.c
+++ src/Config.c
@@ -6,22 +6,41 @@
 #include "includes/Racket.h"
 #include "includes/Ball.h"
 #include "includes/Ground.h"
-#include "includes/Config.h"
 #include "includes/State_Game.h"
+#include "includes/Menu_Item.h"
+#include "includes/State_Menu.h"
+#include "includes/Config.h"
 
-int config_load(State_Game_Env* env_game){
+int config_load(float* config){
 	char* vars[CONFIG_NB] = {
 		"mouse_sensibility=%f\n", 
 		"reflection=%f\n",
 		"particles=%f\n"
 	};
 	int i;
-	FILE* config = fopen("config/config.cfg", "r+");
-	if (!config) {
+	FILE* file_config = fopen("config/config.cfg", "r+");
+	if (!file_config) {
 		return 0;
 	}
 	for (i = 0; i < CONFIG_NB; i++) {
-		fscanf(config, vars[i], &env_game->config[i]);
+		fscanf(file_config, vars[i], &config[i]);
 	}
+	return 1;
+}
+
+int config_load_state_game(State_Game_Env* env){
+	float config[CONFIG_NB];
+	int i;
+	if(config_load(config)){
+		for (i = 0; i < CONFIG_NB; i++) {
+			env->config[i] = config[i];
+		}
+		return 1;
+	}
+	return 0;
+}
+
+int config_load_state_menu(State_Menu_Env* env){
+	
 	return 1;
 }
--- src/includes/Config.h
+++ src/includes/Config.h
@@ -7,5 +7,10 @@
 	#define CONFIG_REFLECTION 			1
 	#define CONFIG_PARTICLES			2
 
+	int config_load(float* config);
+
+	int config_load_state_game(State_Game_Env* env);
+
+	int config_load_state_menu(State_Menu_Env* env);
 
 #endif

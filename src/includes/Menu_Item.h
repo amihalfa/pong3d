7739894b591f062951ac509362e141a1ce282242
Amirouche Halfaoui <amihalfa@users.noreply.github.com>
2011-01-12 11:52:57 +0000

    Correction de la lumière

--- src/includes/Menu_Item.h
+++ src/includes/Menu_Item.h
@@ -5,40 +5,40 @@
 #define MENU_ITEM_CHECKBOX	1
 #define MENU_ITEM_SLIDER	2
 
-#define MENU_ITEM_ALPHA_MIN		0.2f
+#define MENU_ITEM_ALPHA_MIN		0.5f
 
 	typedef struct Menu_Item {
 
 		/* Position */
 		Coord2d position;
-		
+
 		/* Id de l'image affichee dessus */
 		GLuint texture;
-		
+
 		GLfloat anim_step;
-		
+
 		int anim_dir;
-		
+
 		int type;
-		
+
 		int value;
 
 	} Menu_Item;
 
 	/**
 	 *	Dessin d'un item de menu
-	 *	@param menu_item	Reference vers l'item a dessinner	
+	 *	@param menu_item	Reference vers l'item a dessinner
 	 */
 	void menu_item_draw(Menu_Item * menu_item);
-	
+
 	void menu_item_draw_checkbox(Menu_Item* menu_item);
-	
+
 	void menu_item_draw_slider(Menu_Item* menu_item);
-	
+
 	void menu_item_animate(Menu_Item* menu_item, Uint32 e_time);
-	
+
 	void menu_item_animate_to_0(Menu_Item * menu_item, Uint32 e_time);
-	
+
 	int menu_item_mouse_over(Menu_Item* menu_item, Coord2d* mouse);
 
 #endif

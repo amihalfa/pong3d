d082b0eec239c16d1fb2cb6ccfbded9dd62c1eb9
silverna <antoine.hezard>
2011-01-14 00:11:05 +0000

    AI medium et hard done, perfectionnements à faire sans doute mais là on est un peu préssés. J'ai aussi normalisé l'indentation et enfin ajouté la vitesse minimale au balles

--- src/State_Game.c
+++ src/State_Game.c
@@ -35,11 +35,11 @@ State* state_game(int action) {
         SDL_Rect **modes = SDL_ListModes(NULL, SDL_FULLSCREEN | SDL_OPENGL);
         ((State_Game_Env*) state_game->env)->w_height = modes[0]->h;
         ((State_Game_Env*) state_game->env)->w_width = modes[0]->w;
-		((State_Game_Env*) state_game->env)->sound = Mix_LoadWAV("son/collision.wav");
+        ((State_Game_Env*) state_game->env)->sound = Mix_LoadWAV("son/collision.wav");
 
     } else if (action == STATE_DESTROY && state_game) {
 
-		Mix_FreeChunk(((State_Game_Env*) state_game->env)->sound);
+        Mix_FreeChunk(((State_Game_Env*) state_game->env)->sound);
         free(state_game->env);
         free(state_game);
         state_game = (State*) 0;
@@ -68,8 +68,8 @@ void state_game_init(State_Game_Env* env) {
 
     config_load_state_game(env);
 
-	env->heart_texture = util_texture_load("images/game/jaime.png");
-	
+    env->heart_texture = util_texture_load("images/game/jaime.png");
+
     if (sgu_get_pause() == 0) {
 
         /* Mise en place du terrain dans la scene */
@@ -84,7 +84,7 @@ void state_game_init(State_Game_Env* env) {
         env->racket[RACKET_TOP].width = 5.0f;
         env->racket[RACKET_TOP].radius = 0.8f;
         env->racket[RACKET_TOP].speed = 0.0f;
-		env->racket[RACKET_TOP].lifes = 3;
+        env->racket[RACKET_TOP].lifes = 3;
 
         /* ... Raquette du bas ... */
         env->racket[RACKET_BOTTOM] = env->racket[RACKET_TOP];
@@ -94,19 +94,19 @@ void state_game_init(State_Game_Env* env) {
 
         /* Nombre de balles suivant le niveau */
         if (sgu_get_level() == 1) {
-            env->balls_nb = 1;
+            env->balls_nb = 2;
             env->AI_handler = *AI_easy;
         } else if (sgu_get_level() == 2) {
-            env->balls_nb = 5;
+            env->balls_nb = 3;
             env->AI_handler = *AI_medium;
         } else if (sgu_get_level() == 3) {
-            env->balls_nb = 10;
+            env->balls_nb = 4;
             env->AI_handler = *AI_hard;
-        } else if(sgu_get_level() == 4){
-			/* Mode 2 joueurs pas d'IA */
-			env->balls_nb = 1;
-			env->AI_handler = (void*)0;
-		}
+        } else if (sgu_get_level() == 4) {
+            /* Mode 2 joueurs pas d'IA */
+            env->balls_nb = 1;
+            env->AI_handler = (void*) 0;
+        }
 
         env->mouse_motion_x = 0;
         env->mouse_motion_y = 0;
@@ -124,7 +124,7 @@ void state_game_init(State_Game_Env* env) {
             }
             env->ball[i].speed.y = (float) (rand() % 50 + 50) / 1000.0f;
             if (env->ball[i].speed.y < BALL_MIN_SPEED) {
-                    env->ball[i].speed.y = BALL_MIN_SPEED;
+                env->ball[i].speed.y = BALL_MIN_SPEED;
             }
             if (rand() % 2) {
                 env->ball[i].speed.y *= -1;
@@ -174,9 +174,9 @@ void state_game_draw(State_Game_Env* env) {
         ball_draw(&(env->ball[i]));
     }
 
-	util_score_display(env->heart_texture, env->racket[RACKET_TOP].lifes, -env->ground.width/2+1.0f, env->ground.length/2-2.0f);
-	util_score_display(env->heart_texture, env->racket[RACKET_BOTTOM].lifes, -env->ground.width/2+1.0f, -env->ground.length/2+1.0f);
-	
+    util_score_display(env->heart_texture, env->racket[RACKET_TOP].lifes, -env->ground.width / 2 + 1.0f, env->ground.length / 2 - 2.0f);
+    util_score_display(env->heart_texture, env->racket[RACKET_BOTTOM].lifes, -env->ground.width / 2 + 1.0f, -env->ground.length / 2 + 1.0f);
+
     /* Dessin des deux raquettes */
     racket_draw(&(env->racket[RACKET_TOP]));
     racket_draw(&(env->racket[RACKET_BOTTOM]));
@@ -190,32 +190,32 @@ int state_game_events(State_Game_Env* env) {
 
     env->mouse_motion_x = env->mouse_motion_y = 0;
 
-    while (SDL_PollEvent(&event)){
-		switch (event.type) {
-			case SDL_QUIT:
-				return 0;
-				break;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                return 0;
+                break;
 
-			case SDL_MOUSEMOTION:
-				if (sgu_get_pause() == 2) {
-					env->mouse_motion_x += event.motion.xrel;
-					env->mouse_motion_y += event.motion.yrel;
-				}
-				break;
-			case SDL_MOUSEBUTTONDOWN:
-				sgu_set_pause(2);
-				break;
-		}
-	}
-	
+            case SDL_MOUSEMOTION:
+                if (sgu_get_pause() == 2) {
+                    env->mouse_motion_x += event.motion.xrel;
+                    env->mouse_motion_y += event.motion.yrel;
+                }
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                sgu_set_pause(2);
+                break;
+        }
+    }
+
     env->keystates = SDL_GetKeyState(NULL);
 
     if (env->keystates[ SDLK_ESCAPE ]) {
         state_set_current(state_get_menu());
     }
     if (env->keystates[ SDLK_SPACE ]) {
-		sgu_set_pause(2);
-	}
+        sgu_set_pause(2);
+    }
 
     return 1;
 
@@ -224,69 +224,67 @@ int state_game_events(State_Game_Env* env) {
 void state_game_main(State_Game_Env* env, Uint32 e_time) {
 
     int i;
-	
+
     env->ellapsed_time = e_time;
 
     sgu_destroy_balls_out(env);
-	
-	/* On n'utilise pas l'IA dans le mode 2 joueurs */
-	if(sgu_get_level() != 4){
-		env->AI_handler(env, RACKET_TOP);
-	}
-	
+
+    /* On n'utilise pas l'IA dans le mode 2 joueurs */
+    if (sgu_get_level() != 4) {
+        env->AI_handler(env, RACKET_TOP);
+    }
+
     if (sgu_get_pause() != 1) {
-		
-		/* Gestion de la raquette du haut avec le clavier dans le cas ou pas d'IA */
-		if(sgu_get_level() == 4){
-			racket_keyboard_move(env, RACKET_TOP);
-			racket_keyboard_move(env, RACKET_BOTTOM);
-		}
-		else {
-			/* Gestion de la raquette du bas avec la souris */
-			racket_mouse_move(env, RACKET_BOTTOM);
-		}
-		
-		/* On bouge les raquettes */
-		for (i = 0; i < 2; i++) {
+
+        /* Gestion de la raquette du haut avec le clavier dans le cas ou pas d'IA */
+        if (sgu_get_level() == 4) {
+            racket_keyboard_move(env, RACKET_TOP);
+            racket_keyboard_move(env, RACKET_BOTTOM);
+        } else {
+            /* Gestion de la raquette du bas avec la souris */
+            racket_mouse_move(env, RACKET_BOTTOM);
+        }
+
+        /* On bouge les raquettes */
+        for (i = 0; i < 2; i++) {
             racket_move(&env->racket[i]);
         }
-        
+
         /* Gestion des collisions */
         collision_state_game(env);
-		
-		/* Mouvement des balles et de leurs particules */
+
+        /* Mouvement des balles et de leurs particules */
         for (i = 0; i < env->balls_nb; i++) {
             if (env->config[CONFIG_PARTICLES])
                 particles_add_position(&env->ball[i].particles, &env->ball[i].position);
             ball_move(&env->ball[i], e_time);
         }
     }
-	
-	/* Cas ou il n'y a pas de vie d'un cote ou de l'autre */
-	if(env->racket[RACKET_TOP].lifes <= 0 || env->racket[RACKET_BOTTOM].lifes <=0){
-		sgu_set_pause(1);
-	}
-	else {
-		/* Aucune balle sur le terrain 
-		 * Le Niveau 1 est actif ou bien le mode 2 joueurs
-		 * Il y a des vies des 2 cotes
-		 */
-		if (env->balls_nb == 0 && (sgu_get_level() == 1 || sgu_get_level() == 4)) {
-			sgu_set_pause(1);
-			
-			/* On repositionne la balle au centre */
-			env->balls_nb = 1;
-			env->ball[0].position.x = 0;
-			env->ball[0].position.y = 0;
-			
-			/* On repositionne les raquettes au centre */
-			env->racket[RACKET_TOP].position.x = 0;
-			env->racket[RACKET_BOTTOM].position.x = 0;
-			
-			/* On reinitialise les particules derriere la balle */
-			particles_init(&env->ball[i].particles, &env->ball[i].position);
-		}
-	}
+
+    /* Cas ou il n'y a pas de vie d'un cote ou de l'autre */
+    if (env->racket[RACKET_TOP].lifes <= 0 || env->racket[RACKET_BOTTOM].lifes <= 0) {
+        sgu_set_pause(1);
+    } else {
+        /* Aucune balle sur le terrain
+         * Le Niveau 1 est actif ou bien le mode 2 joueurs
+         * Il y a des vies des 2 cotes
+         */
+        if (env->balls_nb == 0 && (sgu_get_level() == 1 || sgu_get_level() == 4)) {
+            sgu_set_pause(1);
+
+            /* On repositionne la balle au centre */
+            env->balls_nb = 1;
+            env->ball[0].position.x = 0;
+            env->ball[0].position.y = 0;
+
+            /* On repositionne les raquettes au centre */
+            env->racket[RACKET_TOP].position.x = 0;
+            env->racket[RACKET_BOTTOM].position.x = 0;
+
+            /* On reinitialise les particules derriere la balle */
+            particles_init(&env->ball[i].particles, &env->ball[i].position);
+        }
+    }
 
     state_game_draw(env);
 }
